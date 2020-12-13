d34aa7ca1b2a997f2c7ebae33a5e6a242c223b69
Akr1j <jirkajandourek@seznam.cz>
2020-12-13 14:50:51 +0100

    Příprava na přidání senzoru BMP280

--- Meteostanice/Meteostanice/src/main.cpp
+++ Meteostanice/Meteostanice/src/main.cpp
@@ -2,6 +2,12 @@
 #include <sys/types.h>
 #include <sys/time.h>
 
+/* LZE OTESTOVAT AŽ PO NAPÁJENÍ BMP280
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+*/
+
 #define dobaSpanku 5 //Doba spánku (ve vteřinách)
 #define rozptylCasu 1 //Rozsah korekce času (časovač se posunuje zde doba kdy se vyhlásí poplach)
 
@@ -15,6 +21,20 @@ boolean dest; //Pokud je senzor mokrý - 1 jinak 0
 static uint64_t sleep_start_us = 0;
 struct timeval tv_now;
 RTC_DATA_ATTR long int casSpankuStary; //hodnota ukládaná i přes deepSleep
+
+
+
+
+/* LZE OTESTOVAT AŽ PO NAPÁJENÍ BMP280
+Adafruit_BMP280 bmp; // use I2C interface
+Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
+Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
+*/
+
+
+
+
+
 
 void kradez(){
   Serial.println("Ukradnuto");
@@ -38,8 +58,65 @@ void timeChecker() {  //Funkce pro kontrolu zda bylo zařízení ukradnuto
 }
 
 
+
+
+
+
+
+
+/*  LZE OTESTOVAT AŽ PO NAPÁJENÍ BMP280
+
+void BMP280Start() {
+  Serial.println(F("BMP280 Sensor event test"));
+
+  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1) delay(10);
+  }
+
+  // Default settings from datasheet.
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Operating Mode.
+                  Adafruit_BMP280::SAMPLING_X2,     // Temp. oversampling 
+                  Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling 
+                  Adafruit_BMP280::FILTER_X16,      // Filtering. 
+                  Adafruit_BMP280::STANDBY_MS_500); // Standby time. 
+
+  bmp_temp->printSensorDetails();
+}
+
+void BMP280Read() {
+  sensors_event_t temp_event, pressure_event;
+  bmp_temp->getEvent(&temp_event);
+  bmp_pressure->getEvent(&pressure_event);
+  
+  Serial.print(F("Temperature = "));
+  Serial.print(temp_event.temperature);
+  Serial.println(" *C");
+
+  Serial.print(F("Pressure = "));
+  Serial.print(pressure_event.pressure);
+  Serial.println(" hPa");
+
+  Serial.println();
+  delay(2000);
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
 void setup() {
   Serial.begin(9600);
+
+  //BMP280Start();  LZE OTESTOVAT AŽ PO NAPÁJENÍ BMP280
 
   pinMode(rainDigital,INPUT);
 }
@@ -51,7 +128,7 @@ void loop() {
   dest = !(digitalRead(rainDigital)); // kontrola zda prší
   Serial.println(dest);
 
-
+  //BMP280Read();   LZE OTESTOVAT AŽ PO NAPÁJENÍ BMP280
   
   esp_deep_sleep(sleepTime * 1000000); //Uspání zařízení na dobu v proměné sleepTime
 }
